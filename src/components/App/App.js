10df227134e6400bb9a83e9d4ffb0c4af1fd38bd
dominiklis <lis.dominik11@gmail.com>
2022-08-31 23:48:51 +0200

    initial load

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,119 @@
-import { RequireAuth, ToastsContainer } from "components";
+import { LoadingPage, RequireAuth, ToastsContainer } from "components";
 import Layout from "components/Layout/Layout";
+import { localStorageKeys } from "constantStrings";
 import { Home, Login, Register } from "pages";
+import { useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
+import {
+  getLabels,
+  getProducts,
+  getShoppingListItems,
+  getShoppingLists,
+  getStorages,
+  renewToken,
+  setGetLabelsLoading,
+  setGetProductsLoading,
+  setGetSettingsLoading,
+  setGetShoppingListItemsLoading,
+  setGetShoppingListsLoading,
+  setGetStoragesLoading,
+  setInitialLoad,
+  setSettings,
+} from "store/actions";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  const {
+    initialLoad,
+    loading: { getSettings: loadingSettings },
+  } = useSelector((state) => state.app);
+
+  const {
+    loading: { getting: loadingStorages },
+  } = useSelector((state) => state.storages);
+
+  const {
+    loading: { getting: loadingProducts },
+  } = useSelector((state) => state.products);
+
+  const {
+    loading: { getting: loadingLabels },
+  } = useSelector((state) => state.labels);
+
+  const {
+    loading: { getting: loadingShoppingLists },
+  } = useSelector((state) => state.shoppingLists);
+
+  const {
+    loading: { getting: loadingShoppingListItems },
+  } = useSelector((state) => state.shoppingListItems);
+
+  const loadApp = useCallback(async () => {
+    const setLoadingsToFalse = () => {
+      dispatch(setGetSettingsLoading(false));
+      dispatch(setGetStoragesLoading(false));
+      dispatch(setGetProductsLoading(false));
+      dispatch(setGetLabelsLoading(false));
+      dispatch(setGetShoppingListsLoading(false));
+      dispatch(setGetShoppingListItemsLoading(false));
+    };
+
+    // authenticate user
+    const userToken = localStorage.getItem(localStorageKeys.userTokenKey);
+    if (userToken) {
+      try {
+        // verify token
+        const result = await dispatch(renewToken()).unwrap();
+
+        // get user resources
+        if (result?.userId) {
+          dispatch(getStorages());
+          dispatch(getProducts());
+          dispatch(getLabels());
+          dispatch(getShoppingLists());
+          dispatch(getShoppingListItems());
+          dispatch(setSettings(result.userId));
+        }
+      } catch (error) {
+        setLoadingsToFalse();
+      }
+    } else {
+      setLoadingsToFalse();
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    loadApp();
+  }, [loadApp]);
+
+  // after loading the content, set initialLoad to true
+  useEffect(() => {
+    if (
+      loadingSettings ||
+      loadingStorages ||
+      loadingProducts ||
+      loadingLabels ||
+      loadingShoppingLists ||
+      loadingShoppingListItems
+    )
+      dispatch(setInitialLoad(false));
+    else dispatch(setInitialLoad(true));
+  }, [
+    dispatch,
+    loadingSettings,
+    loadingStorages,
+    loadingProducts,
+    loadingLabels,
+    loadingShoppingLists,
+    loadingShoppingListItems,
+  ]);
+
+  if (!initialLoad) {
+    return <LoadingPage />;
+  }
+
   return (
     <div>
       <ToastsContainer />
