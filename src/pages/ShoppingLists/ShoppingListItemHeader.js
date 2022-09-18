9485ac3ba7b477126695a60dd5edc2be63cd7bf3
dominiklis <lis.dominik11@gmail.com>
2022-09-18 19:47:49 +0200

    ShoppingListItemHeader

---
+++ src/pages/ShoppingLists/ShoppingListItemHeader.js
@@ -0,0 +1,44 @@
+import { Button } from "components";
+import { componentColors } from "constantStrings";
+import { useShoppingListItemHeader } from "pages/ShoppingLists";
+import React from "react";
+import { IoCheckboxOutline, IoSquareOutline } from "react-icons/io5";
+import styles from "./ShoppingListItemHeader.module.css";
+
+const ShoppingListItemHeader = ({
+  shoppingListItemId,
+  shoppingListItemName,
+  quantity,
+  selected,
+  ownerId,
+  shoppingListId,
+}) => {
+  const { getContainerStyles, darkTheme, handleSubmit, loading } =
+    useShoppingListItemHeader({
+      shoppingListItemId,
+      shoppingListItemName,
+      quantity,
+      selected,
+      ownerId,
+      shoppingListId,
+    });
+
+  return (
+    <div className={getContainerStyles()} data-dark-theme={darkTheme}>
+      <form onSubmit={handleSubmit}>
+        <Button
+          type="submit"
+          iconButton
+          icon={selected ? <IoCheckboxOutline /> : <IoSquareOutline />}
+          onClick={(e) => e.stopPropagation()}
+          loading={loading}
+          backgroundColor={componentColors.transparent}
+        />
+      </form>
+      <span>{shoppingListItemName}</span>
+      <span className={styles.quantity}>{quantity}</span>
+    </div>
+  );
+};
+
+export default ShoppingListItemHeader;
