3eec18ee7fecf7cac8f83256f1d8dd740d8c1047
dominiklis <lis.dominik11@gmail.com>
2022-09-19 03:02:57 +0200

    sorting shopping list items in selector

--- src/store/selectors/shoppingListItems.js
+++ src/store/selectors/shoppingListItems.js
@@ -1,15 +1,28 @@
 import { createSelector } from "@reduxjs/toolkit";
+import { sortByValues } from "constantStrings";
 import { selectShoppingListItems } from "store/selectors";
+import { sortIdsByName } from "utils";
 
 export const makeSelectShoppingListItems = () =>
   createSelector(
     selectShoppingListItems,
     (_, shoppingListId) => shoppingListId,
-    (lists, shoppingListId) => {
+    (items, { shoppingListId, sortBy } = {}) => {
       if (!shoppingListId) shoppingListId = null;
 
-      return lists.allIds
-        .filter((id) => lists.byId[id].shoppingListId === shoppingListId)
-        .map((id) => lists.byId[id]);
+      let results = [];
+
+      results = items.allIds.filter(
+        (id) => items.byId[id].shoppingListId === shoppingListId
+      );
+
+      sortIdsByName(
+        results,
+        items.byId,
+        "shoppingListItemName",
+        sortBy === sortByValues.nameDesc
+      );
+
+      return results.map((item) => items.byId[item]);
     }
   );
