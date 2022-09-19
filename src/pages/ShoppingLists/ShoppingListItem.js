aaf9d1b557c90e03894a3a2313ef681556a1abd2
dominiklis <lis.dominik11@gmail.com>
2022-09-19 03:02:28 +0200

    ShoppingListItem

---
+++ src/pages/ShoppingLists/ShoppingListItem.js
@@ -0,0 +1,21 @@
+import { Accordion } from "components";
+import {
+  ShoppingListItemActions,
+  ShoppingListItemHeader,
+} from "pages/ShoppingLists";
+import React from "react";
+import { useSelector } from "react-redux";
+
+const ShoppingListItem = ({ shoppingListItemId }) => {
+  const shoppingListItem = useSelector(
+    (state) => state.shoppingListItems.byId[shoppingListItemId]
+  );
+
+  return (
+    <Accordion header={<ShoppingListItemHeader {...shoppingListItem} />}>
+      <ShoppingListItemActions {...shoppingListItem} />
+    </Accordion>
+  );
+};
+
+export default ShoppingListItem;
---
+++ src/pages/ShoppingLists/ShoppingListItemActions.js
@@ -0,0 +1,56 @@
+import { Actions, Translate } from "components";
+import {
+  AddItemToStorage,
+  DeleteShoppingListItem,
+  EditShoppingListItem,
+} from "pages/ShoppingLists";
+import React from "react";
+import { IoBagAdd, IoPencil } from "react-icons/io5";
+import { Action } from "utils";
+
+const componentName = "ShoppingListItemActions";
+
+const ShoppingListItemActions = ({
+  shoppingListItemId,
+  shoppingListItemName,
+  quantity,
+  selected,
+  shoppingListId,
+}) => {
+  return (
+    <Actions
+      actions={[
+        new Action(
+          <Translate section={componentName} text="editActionHeader" />,
+          (
+            <EditShoppingListItem
+              shoppingListItemId={shoppingListItemId}
+              shoppingListItemName={shoppingListItemName}
+              quantity={quantity}
+              selected={selected}
+              shoppingListId={shoppingListId}
+            />
+          ),
+          <Translate section={componentName} text="editButtonText" />,
+          <IoPencil />
+        ),
+        new Action(
+          (
+            <Translate
+              section={componentName}
+              text="createProductActionHeader"
+            />
+          ),
+          <AddItemToStorage name={shoppingListItemName} />,
+          <Translate section={componentName} text="createProductButtonText" />,
+          <IoBagAdd />
+        ),
+      ]}
+      additionalButtonsAfter={
+        <DeleteShoppingListItem shoppingListItemId={shoppingListItemId} />
+      }
+    />
+  );
+};
+
+export default ShoppingListItemActions;
