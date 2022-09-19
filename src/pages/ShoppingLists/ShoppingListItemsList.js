8ee7061c687fb49cab94ccaa22f84070d00faf76
dominiklis <lis.dominik11@gmail.com>
2022-09-19 03:03:10 +0200

    listing items in shopping list

---
+++ src/pages/ShoppingLists/ShoppingListItemsList.js
@@ -0,0 +1,30 @@
+import { NoItemsToDisplay, ShoppingListItem } from "pages/ShoppingLists";
+import React from "react";
+
+const ShoppingListItemsList = ({ items }) => {
+  if (!items?.length) {
+    return <NoItemsToDisplay />;
+  }
+
+  return (
+    <ul>
+      {items
+        .filter((item) => !item.selected)
+        .map((item) => (
+          <li key={item.shoppingListItemId}>
+            <ShoppingListItem shoppingListItemId={item.shoppingListItemId} />
+          </li>
+        ))}
+
+      {items
+        .filter((item) => item.selected)
+        .map((item) => (
+          <li key={item.shoppingListItemId}>
+            <ShoppingListItem shoppingListItemId={item.shoppingListItemId} />
+          </li>
+        ))}
+    </ul>
+  );
+};
+
+export default ShoppingListItemsList;
