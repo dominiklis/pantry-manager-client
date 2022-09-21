5a264e1bf76c70264dacee8a32e3044820205e4a
dominiklis <lis.dominik11@gmail.com>
2022-09-21 22:37:56 +0200

    SearchStorages

---
+++ src/pages/Search/SearchStorages.js
@@ -0,0 +1,29 @@
+import { StorageHeader, Translate } from "components";
+import { ItemsList } from "pages/Search";
+import React, { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { useSearchParams } from "react-router-dom";
+import { makeSelectStorages } from "store/selectors";
+
+const componentName = "SearchStorages";
+
+const SearchStorages = () => {
+  let [searchParams] = useSearchParams();
+  let [search] = React.useState(searchParams.get("q"));
+
+  const selectStorages = useMemo(makeSelectStorages, []);
+  const storages = useSelector((prev) => selectStorages(prev, { search }));
+
+  return (
+    <ItemsList
+      header={<Translate section={componentName} text="header" />}
+      items={storages.map((storage) => (
+        <li key={storage.storageId}>
+          <StorageHeader {...storage} />
+        </li>
+      ))}
+    />
+  );
+};
+
+export default SearchStorages;
