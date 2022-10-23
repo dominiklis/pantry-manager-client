46f1a10a7e5f25c38306403147cc1270c5b72e2d
dominiklis <lis.dominik11@gmail.com>
2022-10-23 17:40:29 +0200

    displaying toast after updating lists and items

--- src/components/RefreshListsAndItems/RefreshListsAndItems.js
+++ src/components/RefreshListsAndItems/RefreshListsAndItems.js
@@ -1,7 +1,13 @@
 import { RefreshForm } from "components";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getShoppingListItems, getShoppingLists } from "store/actions";
+import {
+  addToast,
+  getShoppingListItems,
+  getShoppingLists,
+} from "store/actions";
+
+const componentName = "RefreshListsAndItems";
 
 const RefreshListsAndItems = () => {
   const dispatch = useDispatch();
@@ -13,6 +19,15 @@ const RefreshListsAndItems = () => {
 
     await dispatch(getShoppingLists()).unwrap();
     await dispatch(getShoppingListItems()).unwrap();
+
+    dispatch(
+      addToast({
+        translate: {
+          section: componentName,
+          text: "successToastMessage",
+        },
+      })
+    );
   };
 
   return <RefreshForm onSubmit={handleSubmit} loading={loading} />;
