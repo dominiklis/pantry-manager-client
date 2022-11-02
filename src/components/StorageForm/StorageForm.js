785111e59af80d828113b665f543416013c1eb51
dominiklis <lis.dominik11@gmail.com>
2022-11-02 20:33:06 +0100

    use colors in store to create ColorButtons in StorageForm

--- src/components/StorageForm/StorageForm.js
+++ src/components/StorageForm/StorageForm.js
@@ -1,11 +1,8 @@
 import { Button, Dropdown, Input, Label, Translate } from "components";
 import { ColorButton } from "components/StorageForm";
-import {
-  componentColors,
-  componentSizes,
-  storageColors,
-} from "constantStrings";
+import { componentColors, componentSizes } from "constantStrings";
 import React from "react";
+import { useSelector } from "react-redux";
 import { validateInput } from "utils";
 import styles from "./StorageForm.module.css";
 
@@ -20,6 +17,8 @@ const StorageForm = ({
   loading,
   submitButtonText,
 }) => {
+  const { colors } = useSelector((state) => state.app);
+
   const handleChange = (e) => {
     setInput((prev) => ({ ...prev, [e.target.name]: e.target.value }));
 
@@ -69,125 +68,16 @@ const StorageForm = ({
             dropdownButton={<ColorButton color={input.color} />}
             dropdownContent={
               <div className={styles.colorButtonsWrapper}>
-                <ColorButton
-                  color={storageColors.white}
-                  onClick={() => {
-                    handleStorageColorChange(storageColors.white);
-                  }}
-                  size={componentSizes.small}
-                />
-                <ColorButton
-                  color={storageColors.red}
-                  onClick={() => {
-                    handleStorageColorChange(storageColors.red);
-                  }}
-                  size={componentSizes.small}
-                />
-                <ColorButton
-                  color={storageColors.pink}
-                  onClick={() => {
-                    handleStorageColorChange(storageColors.pink);
-                  }}
-                  size={componentSizes.small}
-                />
-                <ColorButton
-                  color={storageColors.purple}
-                  onClick={() => {
-                    handleStorageColorChange(storageColors.purple);
-                  }}
-                  size={componentSizes.small}
-                />
-                <ColorButton
-                  color={storageColors.deepPurple}
-                  onClick={() => {
-                    handleStorageColorChange(storageColors.deepPurple);
-                  }}
-                  size={componentSizes.small}
-                />
-                <ColorButton
-                  color={storageColors.indigo}
-                  onClick={() => {
-                    handleStorageColorChange(storageColors.indigo);
-                  }}
-                  size={componentSizes.small}
-                />
-                <ColorButton
-                  color={storageColors.blue}
-                  onClick={() => {
-                    handleStorageColorChange(storageColors.blue);
-                  }}
-                  size={componentSizes.small}
-                />
-                <ColorButton
-                  color={storageColors.lightBlue}
-                  onClick={() => {
-                    handleStorageColorChange(storageColors.lightBlue);
-                  }}
-                  size={componentSizes.small}
-                />
-                <ColorButton
-                  color={storageColors.cyan}
-                  onClick={() => {
-                    handleStorageColorChange(storageColors.cyan);
-                  }}
-                  size={componentSizes.small}
-                />
-                <ColorButton
-                  color={storageColors.teal}
-                  onClick={() => {
-                    handleStorageColorChange(storageColors.teal);
-                  }}
-                  size={componentSizes.small}
-                />
-                <ColorButton
-                  color={storageColors.green}
-                  onClick={() => {
-                    handleStorageColorChange(storageColors.green);
-                  }}
-                  size={componentSizes.small}
-                />
-                <ColorButton
-                  color={storageColors.lightGreen}
-                  onClick={() => {
-                    handleStorageColorChange(storageColors.lightGreen);
-                  }}
-                  size={componentSizes.small}
-                />
-                <ColorButton
-                  color={storageColors.lime}
-                  onClick={() => {
-                    handleStorageColorChange(storageColors.lime);
-                  }}
-                  size={componentSizes.small}
-                />
-                <ColorButton
-                  color={storageColors.yellow}
-                  onClick={() => {
-                    handleStorageColorChange(storageColors.yellow);
-                  }}
-                  size={componentSizes.small}
-                />
-                <ColorButton
-                  color={storageColors.amber}
-                  onClick={() => {
-                    handleStorageColorChange(storageColors.amber);
-                  }}
-                  size={componentSizes.small}
-                />
-                <ColorButton
-                  color={storageColors.orange}
-                  onClick={() => {
-                    handleStorageColorChange(storageColors.orange);
-                  }}
-                  size={componentSizes.small}
-                />
-                <ColorButton
-                  color={storageColors.deepOrange}
-                  onClick={() => {
-                    handleStorageColorChange(storageColors.deepOrange);
-                  }}
-                  size={componentSizes.small}
-                />
+                {colors.map((color) => (
+                  <ColorButton
+                    key={color}
+                    color={color}
+                    onClick={() => {
+                      handleStorageColorChange(color);
+                    }}
+                    size={componentSizes.small}
+                  />
+                ))}
               </div>
             }
           />
